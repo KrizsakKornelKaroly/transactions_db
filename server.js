29e07dfe9f09e75b782c926dc3e0da91e5d98f2e
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2026-02-25 12:30:05 +0100

    ai style

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ var pool = mysql.createPool({
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
